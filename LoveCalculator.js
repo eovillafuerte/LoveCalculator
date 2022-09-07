0300427b2709f945bd996ce9137a5ea055f7a1dc
eovillafuerte <eovillafuerte@outlook.com>
2022-09-07 17:57:06 -0600

    final commit

--- LoveCalculator.js
+++ LoveCalculator.js
@@ -2,5 +2,5 @@ prompt("What is your name?");
 prompt("What is their name?");
 
 var loveScore = Math.random() * 100;
-loveScore = Math.floor(loveScore);
-console.log(loveScore);
+loveScore = Math.floor(loveScore) + 1;
+alert("Your love score is " + loveScore);
