765ae58aafeba1334662981d2a01abd642697b08
eovillafuerte <eovillafuerte@outlook.com>
2022-09-07 18:14:26 -0600

    bug fix

--- LoveCalculator.js
+++ LoveCalculator.js
@@ -5,7 +5,7 @@ var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore > 70) {
-    alert("Your love score is " + loveScore + "%" + "You love each other like Kanye loves Kanye!");
+    alert("Your love score is " + loveScore + "%" + " You love each other like Kanye loves Kanye!");
 } else {
     alert("Your love score is " + loveScore + "%");
 }
